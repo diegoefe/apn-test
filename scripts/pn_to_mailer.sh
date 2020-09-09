8cfc620a48a4d1aea3264c1a92f79db4d75af92e
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2020-09-09 17:01:27 -0300

    Testing locally

--- scripts/pn_to_mailer.sh
+++ scripts/pn_to_mailer.sh
@@ -6,5 +6,5 @@ curl \
   -X POST \
   -H 'Accept:application/json' \
   -d "{ \"type\": \"notify/ios\", \"params\": { \"event\" : \"NOTIF\", \"event_date\":\"${fecha}\", \"template\":\"notification_simple.txt\", \"custom_data\": { \"camera_name\": \"Mensaje del mailer (${fecha})\" }, \"tokens\" : [ \"${token1}\" ], \"platform\":\"nubicam\" }}" \
-http://127.0.0.1:3000/job
+http://127.0.0.1:3001/job
 
